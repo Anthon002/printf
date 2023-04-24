1b16622e354894ae0524434b456772f175541305
danieljonesobot <daniel.jonesobot@gmail.com>
2023-04-24 16:35:24 -0700

    fix to main.h and _print_num.c, _putchar.c files

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * _putchar - replicate putchar
+ * @c: character
+ * Return: character
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
--- main.h
+++ main.h
@@ -14,8 +14,4 @@ int power(int base, int exp);
 void _print_number(int n, int *count);
 int _print_p(va_list args, int count);
 void _print_uoxX(unsigned int n, unsigned int base, int *count, int is_upper);
-/*Functions to print char, string and percent*/
-int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
 #endif
