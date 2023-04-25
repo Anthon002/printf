1f8b76d7e8ed5b8df34d1270e8e1bb60284cc8e7
danieljonesobot <daniel.jonesobot@gmail.com>
2023-04-25 07:16:59 -0700

    betty fix on _print_S.c

--- _print_S.c
+++ _print_S.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/** _print_S - prints the string
+/* _print_S - prints the string
  * @str: string
  * Return: count
  */
@@ -8,23 +8,23 @@ int _print_S(char *str)
 {
 	int i, count = 0;
 	char hex[3] = {'\0', '\0', '\0'};
-    
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] < 32 || str[i] >= 127)
-        {
-            _putchar('\\');
-            _putchar('x');
-            sprintf(hex, "%02X", str[i]);
-            _putchar(hex[0]);
-            _putchar(hex[1]);
-            count += 4;
-        }
-        else
-        {
-            _putchar(str[i]);
-            count++;
-        }
-    }
-    return (count);
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+		       	_putchar('x');
+			sprintf(hex, "%02X", str[i]);
+			_putchar(hex[0]);
+		       	_putchar(hex[1]);
+			count += 4;
+		}
+		else
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
 }
