d5d7716bf01eed1ff366dd5666f5a5c0c415eb9f
danieljonesobot <daniel.jonesobot@gmail.com>
2023-04-25 06:35:31 -0700

    edit to main.h file

--- main.h
+++ main.h
@@ -8,10 +8,4 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int _strlen(const char *str);
 int print_b(va_list args);
-int _write_buffered(char *buffer, int buf_size);
-int print_integer(va_list args);
-int power(int base, int exp);
-void _print_number(int n, int *count);
-int _print_p(va_list args, int count);
-void _print_uoxX(unsigned int n, unsigned int base, int *count, int is_upper);
 #endif
